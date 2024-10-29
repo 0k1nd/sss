50244e3d431b62e3c22e14126de8a0bad340d080
0k1nd <akindtyrbo@gmail.com>
2024-10-29 08:53:29 +0300

    more beautiful view

--- to do list v2.py
+++ to do list v2.py
@@ -1,36 +1,34 @@
-tasks = {
-'task_1': 1,
-'task_2':'London',
-'task_4':320,
-'task_52':1010,
-'task_7':99,
-'task_9': None,
-'task_89': None,
-'task_90':'G1/0/11'
-}
+tasks = {}
 
-def start():
+def main():
     while True:
-        x = str(input(' add_task \n view_tasks \n delete_task \n exit \n  название действия = '))
-        if x == exit:
+        x = int(input(' 1.add_task \n 2.view_tasks \n 3.delete_task \n 4.exit \n  номер действия = '))
+        if x == 4:
             break
-        def add_task():
-            key = input('введите название задачи: ')
-            value = input('описание задачи: ')
-            tasks[key] = value
-        def view_tasks():
-            for index, (keys, values) in enumerate(tasks.items()):
-                print(f" {index + 1}  {values}")
-        def delete_task():
-            for keys in tasks.items():
-                print(keys)
-                x = input('введите название задачи, которую хотите удалить =')
-                del tasks[x] 
+ 
         match x:
-                case "add_task":
+                case 1:
                     add_task()
-                case "view_tasks":
+                case 2:
                     view_tasks() 
-                case "delete_task":
+                case 3:
                     delete_task()
-start()
+
+
+def add_task():
+    key = input('введите название задачи: ')
+    value = 'описание пустое'
+    tasks[key] = value
+
+
+def view_tasks():
+    for index, (keys, values) in enumerate(tasks.items()):
+        print(f" {index + 1}  {keys}")
+
+
+def delete_task():
+    for keys in tasks.items():
+        print(keys)
+        x = input('введите название задачи, которую хотите удалить =')
+        del tasks[x]
+main()
