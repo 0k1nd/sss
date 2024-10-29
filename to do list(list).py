1df8f484669aebad104bf6f086ba0cb1559ce418
0k1nd <akindtyrbo@gmail.com>
2024-10-29 09:18:11 +0300

    new version with list

---
+++ to do list(list).py
@@ -0,0 +1,36 @@
+tasks = []
+
+def main():
+    while True:
+        x = int(input(' 1.add_task \n 2.view_tasks \n 3.delete_task \n 4.exit \n  номер действия = '))
+        if x == 4:
+            break
+ 
+        match x:
+                case 1:
+                    add_task()
+                case 2:
+                    view_tasks() 
+                case 3:
+                    delete_task()
+        print('-------------')
+
+
+def add_task():
+    task_name = str(input('введите название задачи: '))
+    tasks.append(task_name)
+
+
+def view_tasks():
+    for i, element in enumerate(tasks):
+        print((i + 1), element)
+
+
+def delete_task():
+    for i, element in enumerate(tasks):
+        print((i + 1), element)
+    del_task = int(input('введите название задачи, которую хотите удалить ='))
+    tasks.pop(del_task - 1)
+
+
+main()
