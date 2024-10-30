d0a5d855e7894ee5c9c18940c71718c33ad2b612
0k1nd <akindtyrbo@gmail.com>
2024-10-30 09:23:11 +0300

    new classes in Inheritance

--- 30.10/Inheritance.py
+++ 30.10/Inheritance.py
@@ -1,18 +1,45 @@
 class Building:
     year = None
     city = None
-    def __init__(self, year = None, city =None):
+    
+    def __init__(self, year, city):
         self.year = year
         self.city = city
         
     def get_info(self):
         print('Year:', self.year, 'City:', self.city)
 
-class School(Building):
-    pass        
+
+class House(Building):
+    material = None
+    area = 0
+    color = None
+    floors = 0
+    
+    def __init__(self, year, city, material, area, color, floors):
+        super(House, self).__init__(year, city)
+        self.material = material
+        self.area = area
+        self.color = color
+        self.floors = floors
+        
+    def get_info(self):
+        super().get_info()
+        print('Material:', self.material, 'Area of bUilding:', self.area, 'Color:', self.color, 'Floors:', self.floors)
 
 
-school = School(2000, 'moscow')
-minimarket = Building(2000, 'moscow')
-house = Building(2000, 'moscow')
+class School(House):
+    people = 0
+    def __init__(self, people, year, city):
+        super(School, self).__init__(year, city, material, area, color, floors)
+        self.people = people
+        
+    def get_info(self):
+        super().get_info()
+        print('People:', self.people)
 
+
+    
+
+class shop(House):
+    pass
