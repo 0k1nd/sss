d8a566a63ed3ed76315ac4de707b9425628c9c37
0k1nd <akindtyrbo@gmail.com>
2024-10-30 13:40:54 +0300

    add '---'

---
+++ 30.10/after 14.00/task_1.py
@@ -0,0 +1,45 @@
+class Animal():
+
+    def __init__(self, name, sound):
+        self.name = name
+        self.sound = sound
+        
+    def make_sound(self):
+        print(self.sound.upper())
+        
+    def description(self):
+        print('name:', self.name)
+        print('sound:', self.sound)
+    
+class Dog(Animal):
+    
+    def __init__(self, name, sound, breed, color):
+        super(Dog, self).__init__(name, sound)
+        self.breed = breed
+        self.color = color
+        
+    def description(self):
+        super().description()
+        print('breed:', self.breed)
+        print('color', self.color)
+        
+class Cat(Animal):
+    
+    def __init__(self, name, sound, breed, color):
+        super(Cat, self).__init__(name, sound)
+        self.breed = breed
+        self.color = color
+        
+    def description(self):
+        super().description()
+        print('breed:', self.breed)
+        print('color', self.color)
+        
+        
+cat1 = Cat('Lulu', 'бу испугался не бойся я друг я тебя не обижу', 'Sphinx', 'white')
+cat1.description()
+cat1.make_sound()
+
+dod1 = Dog('qwe', 'gap', 'alabay', 'white')
+dod1.description()
+dod1.make_sound()
