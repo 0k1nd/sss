40a74f71c1407404b36f327a58a0219d0e0c45c5
0k1nd <akindtyrbo@gmail.com>
2024-11-21 14:52:08 +0300

    21.11

--- 21.11/task 1.py
+++ 21.11/task 1.py
@@ -1,32 +1,60 @@
+'''Учитывая n пар круглых скобок, напишите функцию, которая генерирует все комбинации правильных круглых скобок.
+
+Пример 1:
+
+Вход: n = 3
+Выход: ["((()))","(()())","(())()","()(())","()()()"]
+
+Пример 2:
+
+Вход: n = 1
+Выход: ["()"]
+
+
+Ограничения:
+
+    1 <= п <= 8
+'''
+
+
+
 from itertools import permutations
 from itertools import groupby
 from collections import Counter
 
 n = int(input())
-con = (n - 1)
 list_skob = []
 i = 0
 
-while i < con:
+while i < n:
     i += 1
     list_skob.append('(')
     list_skob.append(')')
     
-print(list_skob)
     
 all_variable = list(permutations(list_skob))
 def remove_duplicates(list_of_tuples):
     unique_items = set(list_of_tuples)
     return list(unique_items)
 
-
 result = remove_duplicates(all_variable)
 list_of_variable = []
-for nums in result:
-    str_num = (''.join(nums))
-    vatiant = '({})'
-    rkekgne = vatiant.format(str_num)
-    list_of_variable.append(rkekgne)
+answer = []
+   
+for combination in result:
+    counter = 0
+    list_of_variable.append(combination)
+    for item in combination:
+        if item == ')':
+            counter -= 1
+        elif counter < 0:
+            list_of_variable.remove(combination)
+            break
+        elif item == '(':
+            counter += 1  
     
-print(list_of_variable)
+for nums in list_of_variable:
+    str_num = (''.join(nums))
+    answer.append(str_num) 
 
+print(answer)
